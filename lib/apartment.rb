5e7f7576f0120dfba9523c56be5ed6d5d636035f
Nicola Kim <nicola.copsey@gmail.com>
2020-04-07 16:18:08 -0700

    Iteration 1 complete

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,4 +1,14 @@
 class Apartment
-  def initialize(unit_details)
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+
+  def initialize(apartment_details)
+    @number = apartment_details[:number]
+    @monthly_rent = apartment_details[:monthly_rent]
+    @bathrooms = apartment_details[:bathrooms]
+    @bedrooms = apartment_details[:bedrooms]
+  end
+
+  def add_renter(renter)
+    @renter = renter
   end
 end
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -11,4 +11,29 @@ class ApartmentTest < Minitest::Test
     assert_instance_of Apartment, unit1
   end
 
+  def test_unit_has_a_number
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal "A1", unit1.number
+  end
+
+  def test_unit_has_monthly_rent
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal 1200, unit1.monthly_rent
+  end
+
+  def test_unit_has_bathroom_number
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal 1, unit1.bathrooms
+  end
+
+  def test_unit_has_number_of_bedrooms
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal 1, unit1.bedrooms
+  end
+
+  def test_unit_has_a_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_nil nil, unit1.renter
+  end
+
 end

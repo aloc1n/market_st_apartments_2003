ae686490aac7eaa88928d43e6c1fee9926970816
Nicola Kim <nicola.copsey@gmail.com>
2020-04-06 17:03:31 -0700

    Renter class and tests updated

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -6,9 +6,14 @@ require 'pry'
 
 class RenterTest < Minitest::Test
 
-  def test_it_exists
+  def test_renter_exists
     renter1 = Renter.new("Jessie")
     assert_instance_of Renter, renter1
+  end
+
+  def test_renter_has_a_name
+    renter1 = Renter.new("Jessie")
+    assert_equal "Jessie", renter1.name
   end
 
 end
